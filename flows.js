6dfd5920312ab17bbeedbbf30b5e63b797037bc8
Ying Vuong <yvuong@spokeo.com>
2022-09-30 16:44:57 -0700

    update pages

--- flows.js
+++ flows.js
@@ -5,9 +5,9 @@ async function pageViewFlow(page, context) {
     const baseUrl = context.vars.target;
     const waitTime = 65_000;
 
-    await page.goto(baseUrl);
     await page.goto(`${baseUrl}/contact`);
     await page.goto(`${baseUrl}/privacy`);
+    await page.goto(`${baseUrl}/careers`);
     await wait(waitTime);
     
   } catch (error) {
